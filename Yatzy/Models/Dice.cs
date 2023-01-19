12961401b655ba9eef46642eaf97801fab8e7a76
Hannesnt <hannesnt@gmail.com>
2023-01-19 14:19:50 +0100

    La till tärningar och en roll funktion samt en Hero komponent.

---
+++ Yatzy/Models/Dice.cs
@@ -0,0 +1,17 @@
+﻿namespace Yatzy.Models
+{
+	public class Dice
+	{
+		private Random rand = new Random();
+		public int currentRoll { get; set; }
+		public string SaveRoll { get; set; } = "";
+		public bool currentRollBool { get; set; }
+
+		public int RollDice()
+		{
+			currentRoll = rand.Next(1, 7);
+			return currentRoll;
+		}
+
+	}
+}
