f42cc5e611c7a32bfae37b2ab3613bceda4a5f4b
Dennisfrolander <111590730+Dennisfrolander@users.noreply.github.com>
2023-01-19 09:52:30 +0100

    Lagt till Addplayer-metod och testat den

---
+++ Yatzy/Tests/PlayerMethods.cs
@@ -0,0 +1,15 @@
+﻿using Yatzy.Models;
+
+namespace Yatzy.Tests
+{
+	public class PlayerMethods
+	{
+		public void AddPlayer(string firstName, string lastName, List<Player> PlayersList)
+		{
+			if (!string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName))
+			{
+				PlayersList.Add(new Player(firstName, lastName));
+			}
+		}
+	}
+}
---
+++ Yatzy/Tests/PlayerMethodsTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using Yatzy.Models;
+
+namespace Yatzy.Tests
+{
+	public class PlayerMethodsTest
+	{
+		public List<Player> PlayersListAdd = new();
+		[Fact]
+		public void AddPlayerTest()
+		{
+			var newPlayer = new PlayerMethods();
+			string firstName = "Dennis";
+			string lastName = "Frölander";
+			bool expectedValue = false;
+
+			newPlayer.AddPlayer("Dennis", "Frölander", PlayersListAdd);
+
+			foreach (Player player in PlayersListAdd)
+			{
+				if (player.FirstName == firstName && player.LastName == lastName)
+				{
+					expectedValue = true;
+				}
+				else
+				{
+					expectedValue = false;
+				}
+			}
+
+			Assert.True(expectedValue);
+		}
+	}
+}
